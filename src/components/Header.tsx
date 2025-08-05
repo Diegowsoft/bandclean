721c5afdfceceb8dc3c722f8c3db0bf7328ad977
Diego Fagundes <126366406+Diegowsoft@users.noreply.github.com>
2025-08-04 21:17:53 -0300

    Logo trying to fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -40,9 +40,8 @@ const Header = () => {
           {/* Logo */}
           <div className="flex items-center space-x-2">
             <div className="w-10 h-10 bg-gradient-primary rounded-lg flex items-center justify-center">
-              <span className="text-white font-bold text-lg">BC</span>
+              <img src="../../public/band-clean-logo.png" alt="Band Clean Logo"/>
             </div>
-            <img src="../../public/band-clean-logo.png" alt="Band Clean Logo"/>
           </div>
 
           {/* Desktop Navigation */}
